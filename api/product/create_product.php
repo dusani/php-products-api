f514e62f274e98226c8e8459ebed3044151933ff
Daniel Usani <daniel.e.usani@gmail.com>
2018-04-17 21:00:41 -0400

    Added create_product.php api

--- api/product/create_product.php
+++ api/product/create_product.php
@@ -2,13 +2,13 @@
 // if the form was submitted
 if ($_POST) {
     // include core configuration
-    include_once('../config/core.php');
+    include_once('../../config/core.php');
 
     // include database connection
-    include_once('../config/database.php');
+    include_once('../../config/database.php');
 
     // product object
-    include_once('../objects/product.php');
+    include_once('../../objects/product.php');
 
     // class instance
     $database = new Database();
